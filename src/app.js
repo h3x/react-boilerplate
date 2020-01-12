bd1615055e8df4b0cdf47dde86253e42e55cf9fc
Adam <aausti23@myune.edu.au>
2020-01-12 21:08:20 +1100

    WIP section 1

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// JSX
+const appRoot = document.getElementById("app")
+
+const app = {
+  title: "Indecision App",
+  subtitle: "Put your hands in the life of a computer!"
+}
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ul>
+      <li>list item 1</li>
+      <li>list item 2</li>
+      <li>list item 3</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(template,appRoot);
