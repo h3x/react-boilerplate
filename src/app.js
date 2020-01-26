99db4150abd4ffb542c05cf726bccf2eeca9f66b
Adam <aausti23@myune.edu.au>
2020-01-26 19:05:31 +1100

    version 1 boiler plate done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "normalize.css/normalize.css/";
 import "./styles/styles.scss";
 
-import IndecisionApp from "./components/IndecisionApp";
+import Home from "./components/Home.js";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<Home />, document.getElementById("app"));
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Your new app is ready to be built</h3>
+      </div>
+    );
+  }
+}
