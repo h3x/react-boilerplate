a937d407f3f3b353650f4866a7dfa7f5dadf50c9
Adam <aausti23@myune.edu.au>
2020-01-26 17:52:07 +1100

    section 8 complete

--- src/components/Options.js
+++ src/components/Options.js
@@ -16,9 +16,10 @@ const Options = props => (
     {props.options.length == 0 && (
       <p className="widget__messages">Please add an option to get started!</p>
     )}
-    {props.options.map(option => (
+    {props.options.map((option, index) => (
       <Option
         option={option}
+        count={index + 1}
         handleDeleteOption={props.handleDeleteOption}
         key={Math.random()}
       />
